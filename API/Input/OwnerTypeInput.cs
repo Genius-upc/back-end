798a51296f6d116bd3f55e96cc6896a1cd91e4dd
maiyq1 <U202010305@upc.edu.pe>
2023-06-05 21:56:38 -0500

    OwnerType Controller

---
+++ API/Input/OwnerTypeInput.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Input;
+
+public class OwnerTypeInput
+{
+    [DataType(DataType.Text)]
+    [MaxLength(50)]
+    [MinLength(10)]
+    public string nameType { get; set; }
+    [DataType(DataType.Text)]
+    [MaxLength(50)]
+    [MinLength(10)]
+    public string description { get; set; }
+}
